2af154d52a2679401deb73b96faa6d80cf79516f
noceo <paulschade20@gmail.com>
2022-03-29 01:15:09 -0500

    added font change and font size change functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 //https://www.w3.org/WAI/RD/2012/text-customization/r11
-let background_color = '#FFFFE5';
+let background_color = "#FFFFE5";
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({"background_color": background_color});
+  chrome.storage.sync.set({ background_color: background_color });
+});
+
+chrome.webNavigation.onCompleted.addEventListener(() => {
+  console.log("OPEN");
 });
--- ui/ui.js
+++ ui/ui.js
@@ -1,11 +1,41 @@
-let change_background_button = document.getElementById("change-background");
+const change_background_button = document.getElementById("change-background");
+const change_font_select = document.getElementById("change-font");
+const decrease_font_size = document.getElementById("decrease-font-size");
+const increase_font_size = document.getElementById("increase-font-size");
 
 change_background_button.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: setBackgroundColor,
+  });
+});
+
+change_font_select.addEventListener("change", async (event) => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.storage.sync.set({ font: event.target.value });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setFont,
+    args: [event],
+  });
+});
+
+decrease_font_size.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setFontSize,
+    args: [false],
+  });
+});
+
+increase_font_size.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setFontSize,
+    args: [true],
   });
 });
 
@@ -13,4 +43,36 @@ function setBackgroundColor() {
   chrome.storage.sync.get("background_color", ({ background_color }) => {
     document.body.style.backgroundColor = background_color;
   });
+}
+
+function setFont() {
+  chrome.storage.sync.get("font", ({ font }) => {
+    const elements = document.querySelectorAll("p, span, a, li");
+    if (font === "---") {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontFamily = "";
+      }
+    } else {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontFamily = `${font}`;
+      }
+    }
+  });
+}
+
+function setFontSize(increase) {
+  let font_size;
+  const elements = document.querySelectorAll("p, span, a, li");
+  for (var i = 0; i < elements.length; i++) {
+    font_size = window
+      .getComputedStyle(elements[i], null)
+      .getPropertyValue("font-size");
+    // TODO: include check if a valid font-size is determined
+    font_size = parseFloat(font_size);
+    if (increase) {
+      elements[i].style.fontSize = `${font_size + 1}px`;
+    } else {
+      elements[i].style.fontSize = `${font_size - 1}px`;
+    }
+  }
 }
